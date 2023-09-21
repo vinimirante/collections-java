3e54febac1b0762ed6b568f0d20717a78b939c6c
Vinicius Nascimento <vinicius.nascimento@mirante.com.br>
2023-09-21 18:01:29 -0300

    Initial commit

---
+++ src/edu/java/collections/list/operacoesBasicas/Tarefa.java
@@ -0,0 +1,23 @@
+package edu.java.collections.list.operacoesBasicas;
+
+public class Tarefa {
+
+//    ATRIBUTO
+
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa{" +
+                "descricao='" + descricao + '\'' +
+                '}';
+    }
+}
